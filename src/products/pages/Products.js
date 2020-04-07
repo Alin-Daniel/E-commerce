db5c61ec8df11bc6d8960009756e67c58d55ecec
Alin-Daniel <alin_dani3l@yahoo.com>
2020-04-07 21:45:11 +0300

    improved interface

--- src/products/pages/Products.js
+++ src/products/pages/Products.js
@@ -2,16 +2,27 @@ import React, { useState } from "react";
 
 import ProductsList from "../components/ProductsList";
 import DUMMY_PRODUCTS from "../../shared/DUMMY_PRODUCTS";
-import ProductsFilter from '../components/ProductsFilter';
+import Input from "../../shared/components/FormElements/Input";
+import Colors from "../../shared/components/UIElements/Colors";
 import "./Products.scss";
 
 const Products = (props) => {
   const [products, setProducts] = useState(DUMMY_PRODUCTS);
   return (
-    <section className='collection'>
-      <ProductsFilter />
-      <ProductsList products={products} />
-    </section>
+    <React.Fragment>
+      <section className="collection">
+        <div className="sort-by">
+          <Input type="select" />
+        </div>
+        <div className="filters">
+          <Input type="radio" />
+          <Colors />
+        </div>
+        <div className="collection__gallery">
+          <ProductsList products={products} />
+        </div>
+      </section>
+    </React.Fragment>
   );
 };
 
